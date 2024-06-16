8f7915f4a0ad6d32c69582b5fb3c2632a71212f3
Son421 <115635389+Son421@users.noreply.github.com>
2024-06-16 18:51:45 +0300

    bcryptjs fix

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../modules/Users');
 const Goal = require('../modules/Goal');
